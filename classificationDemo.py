dac5d8a683db35c4c0bc2f8538541581b6825026
Jeff Tao <touchxyz@163.com>
2018-06-14 20:07:22 +0800

    regression

--- classificationDemo.py
+++ classificationDemo.py
@@ -5,3 +5,20 @@ clf = tree.DecisionTreeClassifier()
 clf = clf.fit(X, Y)
 predictedCluster = clf.predict([[2., 2.]])
 print(predictedCluster)
+
+# iris database
+from sklearn.datasets import load_iris
+from sklearn import tree
+iris = load_iris()
+clf = tree.DecisionTreeClassifier()
+clf = clf.fit(iris.data, iris.target)
+predictedCluster = clf.predict(iris.data[:1, :])
+print(predictedCluster)
+
+# Below is an example graphviz export of the above tree trained 
+# on the entire iris dataset; the results are saved in an output file iris.pdf:
+import graphviz 
+dot_data = tree.export_graphviz(clf, out_file=None) 
+graph = graphviz.Source(dot_data) 
+graph.render("iris") 
+graph.view
